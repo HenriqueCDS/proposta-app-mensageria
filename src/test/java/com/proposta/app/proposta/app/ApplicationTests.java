73932a31fa42f0cfb87f16ab13054b7c32dd2bfb
henrique <henriquecordeiro054@gmail.com>
2026-06-15 13:48:58 -0300

    upload spring files

---
+++ src/test/java/com/proposta/app/proposta/app/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proposta.app.proposta.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
