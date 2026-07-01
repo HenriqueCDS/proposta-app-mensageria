0cbbca869c53358949fe25bf0833b150201af8a4
henrique <henriquecordeiro054@gmail.com>
2026-07-01 15:58:26 -0300

    refix/updates modelos pedido/notificação and dockfile-pedidos

--- modulo-pedido/src/main/java/com/proposta/app/proposta/app/service/NotificacaoService.java
+++ modulo-pedido/src/main/java/com/proposta/app/proposta/app/service/NotificacaoService.java
@@ -1,6 +1,5 @@
 package com.proposta.app.proposta.app.service;
 
-import com.proposta.app.proposta.app.dto.PropostaResponseDto;
 import com.proposta.app.proposta.app.entity.Proposta;
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -8,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
 @Service
-public class NotficacaoService {
+public class NotificacaoService {
     private RabbitTemplate rabbitTemplate;
 
     public void notificar(Proposta proposta, String exchange){
